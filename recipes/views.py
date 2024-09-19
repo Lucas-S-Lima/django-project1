df4ef5fdc41888d929154611279cc045429c9692
Lucas da Silva Lima <2ucas2ima@gmail.com>
2024-09-19 20:17:55 -0300

    cleaning unusable archives

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     return render(request, 'recipes/home.html', context=
                   {'message': 'OK'}, status=201)
-
-
-def sobre(request):
-    return HttpResponse('sobre')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
 
 
 
